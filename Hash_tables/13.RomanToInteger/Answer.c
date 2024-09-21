4e00ab178a87d70714a2542e136e7b4564ac6383
zhenanhung <a1597533200@gmail.com>
2024-09-21 15:10:47 +0800

    13.RomanToInteger
    Signed-off-by: zhenanhung <a1597533200@gmail.com>

---
+++ Hash_tables/13.RomanToInteger/Answer.c
@@ -0,0 +1,36 @@
+// two function to implement  this problem
+// 1. check string and transfer to numeral
+// 2. check numeral and transfer to string
+
+int value(char r) {
+    switch(r) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char* s) {
+    int sum = 0;
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        int s1 = value(s[i]);
+        if (i+1 < len) {
+            int s2 = value(s[i+1]);
+            if (s1 >= s2)
+                sum += s1;
+            else {
+                sum += (s2 - s1);
+                i++; 
+            }
+        } else {
+            sum += s1;
+        }
+    }
+    return sum;
+}
